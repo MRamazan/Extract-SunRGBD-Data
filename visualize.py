d1f8d1ce4290a01bcf2c6b3352d38ee4b99edb3e
MRamazan <mramazantrn28@gmail.com>
2025-05-04 00:04:27 +0300

    Update visualize.py

--- visualize.py
+++ visualize.py
@@ -1,67 +1,51 @@
-from sun_utils import Visualize
 import numpy as np
 import open3d as o3d
-image_index = 1 #Dont set this to 0 because there is no file named 000000.txt
-visualize = Visualize()
+import argparse
+from sun_utils import Visualize
 
-rgb_image = visualize.get_rgb_image(image_index)
-depth = visualize.get_depth_image(image_index)
-calib = visualize.get_calib(image_index)
-Rtilt = calib["Rtilt"]
-K = calib["K"]
+def main():
+    # Argparse ile komut satırı argümanlarını al
+    parser = argparse.ArgumentParser(description="Visualize 3D bounding boxes and point cloud data.")
+    parser.add_argument("--image_index", type=int, help="Index of the image to visualize")
+    parser.add_argument("--visualize_function", type=int, choices=[1, 2], help="Select visualization function: 1 for 3D BBoxes on 2D Image, 2 for Point Cloud and BBoxes")
 
-pcd = visualize.get_pcd(image_index)
-objects = visualize.get_label_objects(image_index)
-corners3d = visualize.get_corners_3d(objects)
+    args = parser.parse_args()
 
-def VISUALIZE_3D_BBOXES_ON_2D_IMAGE(rgb_image,corners3d, K, Rtilt):
-    visualize.visualize_3d_bboxes_on_2d_image(rgb_image, corners3d, K, Rtilt)
+    # Visualize nesnesi
+    visualize = Visualize()
 
-def VISUALIZE_POINT_CLOUD_DATA_AND_BBOXES(pcd, corners3d):
-    points = pcd[:, 0:3]
-    color = pcd[:, 3:6]
-    point_cloud = o3d.geometry.PointCloud()
+    # Görselleri ve kalibrasyonu al
+    rgb_image = visualize.get_rgb_image(args.image_index)
+    depth = visualize.get_depth_image(args.image_index)
+    calib = visualize.get_calib(args.image_index)
+    Rtilt = calib["Rtilt"]
+    K = calib["K"]
 
-    point_cloud.points = o3d.utility.Vector3dVector(np.array(points))
-    point_cloud.colors = o3d.utility.Vector3dVector(np.array(color))
-    visualize.visualize_point_cloud_data_and_bboxes(point_cloud, corners3d)
+    # PCD ve 3D köşeleri al
+    pcd = visualize.get_pcd(args.image_index)
+    objects = visualize.get_label_objects(args.image_index)
+    corners3d = visualize.get_corners_3d(objects)
 
-'''VISUALIZE POINT CLOUD DATA AND BBOXES
-   Required folders:  Extract-SunRGBD-Data/calib, Extract-SunRGBD-Data/label,Extract-SunRGBD-Data/pcd'''
-#VISUALIZE_POINT_CLOUD_DATA_AND_BBOXES(pcd, corners3d)
+    # Fonksiyonlar
+    def visualize_3d_bboxes_on_2d_image(rgb_image, corners3d, K, Rtilt):
+        visualize.visualize_3d_bboxes_on_2d_image(rgb_image, corners3d, K, Rtilt)
 
-'''VISUALIZE 3D BBOXES ON 2D IMAGE
-   Required folders:  Extract-SunRGBD-Data/image, Extract-SunRGBD-Data/label, Extract-SunRGBD-Data/calib'''
-#VISUALIZE_3D_BBOXES_ON_2D_IMAGE(rgb_image, corners3d, K, Rtilt)
+    def visualize_point_cloud_data_and_bboxes(pcd, corners3d):
+        points = pcd[:, 0:3]
+        color = pcd[:, 3:6]
+        point_cloud = o3d.geometry.PointCloud()
 
+        point_cloud.points = o3d.utility.Vector3dVector(np.array(points))
+        point_cloud.colors = o3d.utility.Vector3dVector(np.array(color))
+        visualize.visualize_point_cloud_data_and_bboxes(point_cloud, corners3d)
 
+    # Seçilen görselleştirme fonksiyonuna göre işlem yap
+    if args.visualize_function == 1:
+        print("Visualizing 3D BBoxes on 2D image...")
+        visualize_3d_bboxes_on_2d_image(rgb_image, corners3d, K, Rtilt)
+    elif args.visualize_function == 2:
+        print("Visualizing Point Cloud and BBoxes...")
+        visualize_point_cloud_data_and_bboxes(pcd, corners3d)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    main()
